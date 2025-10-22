763567bf0cbb837671bf0663788b65055e1aa496
ezequiel <ezequielcerrato@gmail.com>
2025-10-22 12:34:19 -0300

    Mobile styling support via responsive. Good looking although still wip

--- src/app/components/main-layout/main-layout.ts
+++ src/app/components/main-layout/main-layout.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TodoList } from '../todo-list/todo-list';
-import { TodoHelperComponent } from "../todo-helper-component/todo-helper-component";
-import { ThemeSelector } from "../theme-selector/theme-selector/theme-selector";
-import { RouterOutlet } from "@angular/router";
+import { RouterOutlet } from '@angular/router';
+import { ThemeSelector } from '../theme-selector/theme-selector/theme-selector';
 
 @Component({
   selector: 'app-main-layout',
   imports: [TodoList, ThemeSelector, RouterOutlet],
   templateUrl: './main-layout.html',
-  styleUrl: './main-layout.css'
+  styleUrls: ['./main-layout.css'],
 })
 export class MainLayout {
+  sidebarOpen = signal(false);
+  isMobile = signal(false);
 
+  constructor() {
+    this.checkScreen();
+
+    // Detecta cambios de tamaño de ventana
+    window.addEventListener('resize', () => {
+      this.checkScreen();
+    });
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen.set(!this.sidebarOpen());
+  }
+
+  private checkScreen() {
+    this.isMobile.set(window.innerWidth < 640);
+  }
 }
