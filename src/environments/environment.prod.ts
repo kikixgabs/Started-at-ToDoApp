81d0a592f9afe829de812e69e882e2377053a013
ezequiel <ezequielcerrato@gmail.com>
2025-11-05 12:44:35 -0300

    Trying to configurate for backend connections.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://auth-backend-production-7017.up.railway.app'
+    apiUrl: 'https://https://auth-backend-production-414c.up.railway.app'
 }
