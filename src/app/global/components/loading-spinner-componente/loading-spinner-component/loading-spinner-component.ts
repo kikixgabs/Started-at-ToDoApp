5bc540dbfe2b83d17d4d79c03b210adedcaf7b6a
ezequiel <ezequielcerrato@gmail.com>
2025-11-14 15:15:58 -0300

    Fixed loading spinner

--- src/app/global/components/loading-spinner-componente/loading-spinner-component/loading-spinner-component.ts
+++ src/app/global/components/loading-spinner-componente/loading-spinner-component/loading-spinner-component.ts
@@ -7,8 +7,7 @@ import { LoadingService } from '../../../services/Loading-Service/loading-servic
   imports: [],
   template: `
   @if(loadingService.loading()){
-    <div>" 
-         class="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center z-[9999]">
+    <div class="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center z-[9999]"> 
       <div class="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
     </div>
   }
